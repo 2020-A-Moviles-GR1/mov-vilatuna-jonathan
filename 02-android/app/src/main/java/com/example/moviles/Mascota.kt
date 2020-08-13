323bdbb35391f155630f6f9a259e791bbbcf34bb
Jonathan Vilatuña <jonathan.vilatuna@epn.edu.ec>
2020-08-13 08:07:36 -0500

    crear backend

---
+++ 02-android/app/src/main/java/com/example/moviles/Mascota.kt
@@ -0,0 +1,35 @@
+package com.example.moviles
+
+import android.os.Parcel
+import android.os.Parcelable
+
+//Mascota.kt
+class Mascota (
+    var nombre: String?,
+    var duenio: Usuario?
+):Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readParcelable(Usuario::class.java.classLoader)
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nombre)
+        parcel.writeParcelable(duenio, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Mascota> {
+        override fun createFromParcel(parcel: Parcel): Mascota {
+            return Mascota(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Mascota?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
